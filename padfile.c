3833684f44b367dfbf13d3ce6f54471eb338dc98
Cian Gannon <cianmagfhionnain@outlook.com>
2019-03-11 12:51:54 +0000

    Padding.c done

--- padfile.c
+++ padfile.c
@@ -4,9 +4,25 @@
 union msgblock {
     uint8_t e[64];
     uint32_t t[16];
-    uint_64_t s[8];
+    uint64_t s[8];
 };
 
 int main(int argc, char *argv[]) {
 
+    union msgblock M;
+
+    uint64_t nobytes;
+
+    FILE* f;
+    
+    f = fopen(argv[1], "r");
+
+    while (!feof(f)) {
+        nobytes = fread(M.e, 1, 64, f);
+        printf("%11u\n", nobytes);
+    }
+
+    fclose(f);
+
+    return 0;
 }
