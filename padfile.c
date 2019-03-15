8df270f076beb2382a4f9ae1de7706cdae3d8a38
Cian Gannon <cianmagfhionnain@outlook.com>
2019-03-15 15:43:00 +0000

    Finished padding file

--- padfile.c
+++ padfile.c
@@ -7,6 +7,8 @@ union msgblock {
     uint64_t s[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]) {
 
     union msgblock M;
@@ -15,10 +17,14 @@ int main(int argc, char *argv[]) {
 
     uint64_t nobytes;
 
+    enum status S = READ;
+
     FILE* f;
     f = fopen(argv[1], "r");
 
-    while (!feof(f)) {
+    int i;
+
+    while (S == READ) {
         nobytes = fread(M.e, 1, 64, f);
         printf("Read %2llu bytes\n", nobytes);
         nobits = nobits + (nobytes * 8);
@@ -30,8 +36,26 @@ int main(int argc, char *argv[]) {
                 M.e[nobytes] = 0x00;
             }
             M.s[7] = nobits;
+            S = FINISH;
+        } else if (nobytes < 64) {
+            S = PAD0;
+            M.e[nobytes] = 0x80;
+            while (nobytes < 64) {
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x80;
+            }
+        } else if (feof(f)) {
+            S = PAD1;
         }
     }
+
+    if (S == PAD0 || S == PAD1) {
+        for (i = 0; i < 56; i++) 
+            M.e[i] = 0x80;
+        M.s[7] = nobits;
+    }
+    if (S == PAD1) 
+        M.e[0] = 0x80;
 
     fclose(f);
 
